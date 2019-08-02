12ff69afe5dac4b9425b961e2bd8dbbcc763c567
TJUNLP <jiashengbin0501@163.com>
2019-08-02 11:31:38 +0800

    8-2-1

--- NNstruc/NN_Siamese_SentPair.py
+++ NNstruc/NN_Siamese_SentPair.py
@@ -12,6 +12,121 @@ from keras.layers import merge, Lambda,Flatten
 from keras.layers.merge import dot, Dot
 
 
+def Model_BiLSTM_SentPair_RelPunish_2(wordvocabsize, posivocabsize, charvocabsize, tagvocabsize,
+                     word_W, posi_W, char_W, tag_W,
+                     input_sent_lenth, input_maxword_length,
+                     w2v_k, posi2v_k, c2v_k, tag2v_k,
+                    batch_size=32):
+
+    word_input_sent_x1 = Input(shape=(input_sent_lenth,), dtype='int32')
+    word_input_sent_x2 = Input(shape=(input_sent_lenth,), dtype='int32')
+    word_embedding_sent_layer = Embedding(input_dim=wordvocabsize + 1,
+                                    output_dim=w2v_k,
+                                    input_length=input_sent_lenth,
+                                    mask_zero=True,
+                                    trainable=True,
+                                    weights=[word_W])
+    word_embedding_sent_x1 = word_embedding_sent_layer(word_input_sent_x1)
+    word_embedding_sent_x1 = Dropout(0.25)(word_embedding_sent_x1)
+
+    word_embedding_sent_x2 = word_embedding_sent_layer(word_input_sent_x2)
+    word_embedding_sent_x2 = Dropout(0.25)(word_embedding_sent_x2)
+
+    char_input_sent_x1 = Input(shape=(input_sent_lenth, input_maxword_length,), dtype='int32')
+    char_input_sent_x2 = Input(shape=(input_sent_lenth, input_maxword_length,), dtype='int32')
+    char_embedding_sent_layer = TimeDistributed(Embedding(input_dim=charvocabsize,
+                               output_dim=c2v_k,
+                               batch_input_shape=(batch_size, input_sent_lenth, input_maxword_length),
+                               mask_zero=False,
+                               trainable=True,
+                               weights=[char_W]))
+
+    char_embedding_sent_x1 = char_embedding_sent_layer(char_input_sent_x1)
+    char_embedding_sent_x2 = char_embedding_sent_layer(char_input_sent_x2)
+
+    char_cnn_sent_layer = TimeDistributed(Conv1D(50, 3, activation='relu', padding='valid'))
+    char_embedding_sent_x1 = char_cnn_sent_layer(char_embedding_sent_x1)
+    char_embedding_sent_x1 = TimeDistributed(GlobalMaxPooling1D())(char_embedding_sent_x1)
+    char_embedding_sent_x1 = Dropout(0.25)(char_embedding_sent_x1)
+
+    char_embedding_sent_x2 = char_cnn_sent_layer(char_embedding_sent_x2)
+    char_embedding_sent_x2 = TimeDistributed(GlobalMaxPooling1D())(char_embedding_sent_x2)
+    char_embedding_sent_x2 = Dropout(0.25)(char_embedding_sent_x2)
+
+    input_e1_posi_x1 = Input(shape=(input_sent_lenth,), dtype='int32')
+    input_e1_posi_x2 = Input(shape=(input_sent_lenth,), dtype='int32')
+    input_e2_posi_x1 = Input(shape=(input_sent_lenth,), dtype='int32')
+    input_e2_posi_x2 = Input(shape=(input_sent_lenth,), dtype='int32')
+
+    embedding_posi_layer = Embedding(input_dim=posivocabsize,
+                                    output_dim=posi2v_k,
+                                    input_length=input_sent_lenth,
+                                    mask_zero=False,
+                                    trainable=False,
+                                    weights=[posi_W])
+
+    embedding_e1_posi_x1 = embedding_posi_layer(input_e1_posi_x1)
+    embedding_e1_posi_x2 = embedding_posi_layer(input_e1_posi_x2)
+    embedding_e2_posi_x1 = embedding_posi_layer(input_e2_posi_x1)
+    embedding_e2_posi_x2 = embedding_posi_layer(input_e2_posi_x2)
+
+    input_tag = Input(shape=(1,), dtype='int32')
+    tag_embedding = Embedding(input_dim=tagvocabsize,
+                                    output_dim=tag2v_k,
+                                    input_length=1,
+                                    mask_zero=False,
+                                    trainable=False,
+                                    weights=[tag_W])(input_tag)
+    tag_embedding = Flatten()(tag_embedding)
+
+
+    BiLSTM_layer = Bidirectional(LSTM(200, activation='tanh'), merge_mode='concat')
+
+    embedding_x1 = concatenate([word_embedding_sent_x1, char_embedding_sent_x1,
+                                embedding_e1_posi_x1, embedding_e2_posi_x1], axis=-1)
+    BiLSTM_x1 = BiLSTM_layer(embedding_x1)
+    BiLSTM_x1 = Dropout(0.25)(BiLSTM_x1)
+
+    embedding_x2 = concatenate([word_embedding_sent_x2, char_embedding_sent_x2,
+                                embedding_e1_posi_x2, embedding_e2_posi_x2], axis=-1)
+    BiLSTM_x2 = BiLSTM_layer(embedding_x2)
+    BiLSTM_x2 = Dropout(0.25)(BiLSTM_x2)
+
+    # attention_self = Dense(1, activation='tanh')(BiLSTM_x1)
+    # # attention_probs = Activation('softmax')(attention_concat)
+    # attention_probs = Dense(1, activation='softmax')(attention_self)
+    # # attention_probs = Flatten()(attention_probs)
+    # # attention_multi = Lambda(lambda x: (x[0] + x[1])*0.5)([attention_self, attention_hard])
+    # representation = Lambda(lambda x: x[0] * x[1])([attention_probs, BiLSTM_x1])
+    # attention_x1 = Dropout(0.25)(representation)
+    #
+    # mlp_x2_0 = Flatten()(tag_embedding)
+    # mlp_x1_1 = Dense(200, activation='tanh')(attention_x1)
+    # mlp_x1_1 = Dropout(0.25)(mlp_x1_1)
+    # mlp_x1_2 = Dense(100, activation='tanh')(mlp_x1_1)
+    # # mlp_x1_2 = Dropout(0.25)(mlp_x1_2)
+
+    # distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([BiLSTM_x1, mlp_x2_2])
+    # cos_distance = dot([BiLSTM_x1, BiLSTM_x2], axes=-1, normalize=True)
+    output1 = Dot(axes=-1, normalize=True, name='Output1')([BiLSTM_x1, BiLSTM_x2])
+
+    sent_x1_mlp1 = Dense(200, activation='tanh')(BiLSTM_x1)
+    sent_x1_mlp1 = Dropout(0.25)(sent_x1_mlp1)
+    sent_x1_mlp2 = Dense(100, activation='tanh')(sent_x1_mlp1)
+    relPunish = Dot(axes=-1, normalize=True, name='relPunish')([sent_x1_mlp2, tag_embedding])
+
+
+    mymodel = Model([word_input_sent_x1, input_e1_posi_x1, input_e2_posi_x1, char_input_sent_x1,
+                     word_input_sent_x2, input_e1_posi_x2, input_e2_posi_x2, char_input_sent_x2, input_tag],
+                    [output1, relPunish])
+
+    mymodel.compile(loss={'Output1': anti_contrastive_loss, 'relPunish': cos_distance_loss},
+                    loss_weights={'Output1': 1., 'relPunish': 0.5},
+                    optimizer=optimizers.Adam(lr=0.001),
+                    metrics=[acc_siamese])
+
+    return mymodel
+
 
 def Model_BiLSTM_SentPair_RelPunish_1(wordvocabsize, posivocabsize, charvocabsize, tagvocabsize,
                      word_W, posi_W, char_W, tag_W,
@@ -343,12 +458,19 @@ def contrastive_loss(y_true, y_pred):
     return K.mean(y_true * K.square(y_pred) +
                   (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
 
+
 def anti_contrastive_loss(y_true, y_pred):
 
     margin = 0.5
     return K.mean((1 - y_true) * K.square(y_pred) +
                   y_true * K.square(1 - y_pred))
 
+
+def cos_distance_loss(y_true, y_pred):
+
+    # return K.sum(K.square(1 - y_pred), axis=1, keepdims=True)
+    margin = 0.8
+    return K.sqrt(K.sum(K.square(K.maximum(margin - y_pred, K.epsilon())), axis=1, keepdims=True))
 
 
 def Model_3Level_tag2v(wordvocabsize, targetvocabsize, charvobsize, posivocabsize,
