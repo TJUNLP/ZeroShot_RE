5ae801376e82471910120b736a739c0ca533ec35
TJUNLP <jiashengbin0501@163.com>
2019-09-12 10:02:16 +0800

    9-12-2

--- TrainModel_Siamese_SentPair_5.py
+++ TrainModel_Siamese_SentPair_5.py
@@ -531,7 +531,7 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                                                   input_maxword_length=max_c,
                                                   w2v_k=w2v_k, posi2v_k=posi2v_k, c2v_k=c2v_k, tag2v_k=tag2v_k,
                                                   batch_size=batch_size)
-    elif modelname is 'Model_BiLSTM_SentPair_RelPunish_4_002':
+    elif modelname is 'Model_BiLSTM_SentPair_RelPunish_4_0002':
         nn_model = Model_BiLSTM_SentPair_RelPunish_4(wordvocabsize=wordvocabsize,
                                                   posivocabsize=posivocabsize,
                                                   charvocabsize=charvocabsize,
