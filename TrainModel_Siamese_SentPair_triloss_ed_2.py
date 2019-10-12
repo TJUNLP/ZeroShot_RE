873cefe31f6eeefbc3e1ae322ee91359ad896fdc
TJUNLP <jiashengbin0501@163.com>
2019-10-12 22:01:05 +0800

    10-12-1

--- TrainModel_Siamese_SentPair_triloss_ed_2.py
+++ TrainModel_Siamese_SentPair_triloss_ed_2.py
@@ -13,7 +13,7 @@ import os.path
 import numpy as np
 import ProcessData_Siamese_SentPair
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from NNstruc.NN_Siamese_SentTriplet import Model_BiLSTM_SentPair_tripletloss_1
+from NNstruc.NN_Siamese_SentTriplet import Model_BiLSTM_SentPair_Atloss_ed_05
 from NNstruc.NN_Siamese_SentTriplet import Model_BiLSTM_SentPair_tripletloss_ed_1_01
 import keras
 
@@ -442,8 +442,8 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                                                   input_maxword_length=max_c,
                                                   w2v_k=w2v_k, posi2v_k=posi2v_k, c2v_k=c2v_k, tag2v_k=tag2v_k,
                                                   batch_size=batch_size)
-    elif modelname is 'Model_BiLSTM_SentPair_tripletloss_05':
-        nn_model = Model_BiLSTM_SentPair_tripletloss_1(wordvocabsize=wordvocabsize,
+    elif modelname is 'Model_BiLSTM_SentPair_Atloss_ed_05':
+        nn_model = Model_BiLSTM_SentPair_Atloss_ed_05(wordvocabsize=wordvocabsize,
                                                   posivocabsize=posivocabsize,
                                                   charvocabsize=charvocabsize,
                                                     tagvocabsize=tagvocabsize,
@@ -464,7 +464,7 @@ def Dynamic_get_trainSet(istest):
     else:
         tagDict = tagDict_train
 
-    pairs_train, labels_train = ProcessData_Siamese_SentPair.CreateTriplet_withSoftmax(tagDict, target_vob=None, istest=istest)
+    pairs_train, labels_train = ProcessData_Siamese_SentPair.CreateTriplet_withSoftmax(tagDict, target_vob=target_vob, istest=istest)
     print('CreatePairs train len = ', len(pairs_train[0]), len(labels_train))
 
 
@@ -502,6 +502,7 @@ if __name__ == "__main__":
     modelname = 'Model_BiLSTM_SentPair_tripletloss_05'
     modelname = 'Model_BiLSTM_SentPair_tripletloss_ed'
     modelname = 'Model_BiLSTM_SentPair_tripletloss_ed_1_01'
+    modelname = 'Model_BiLSTM_SentPair_Atloss_ed_05'
 
     print(modelname)
 
