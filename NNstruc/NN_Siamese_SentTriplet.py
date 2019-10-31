61c4597957fe8e8b5506cb8eafdba3309be4b885
TJUNLP <jiashengbin0501@163.com>
2019-10-31 22:17:33 +0800

    10-31-13

--- NNstruc/NN_Siamese_SentTriplet.py
+++ NNstruc/NN_Siamese_SentTriplet.py
@@ -1672,8 +1672,8 @@ def Model_BiLSTM_SentPair_Atloss_ed_05_pinjie(wordvocabsize, posivocabsize, char
     BiLSTM_x3 = concatenate([forward_h_x3, backward_h_x3], axis=-1)
     BiLSTM_x3 = Dropout(0.25)(BiLSTM_x3)
 
-    class_pinjie = concatenate([tag_embedding, BiLSTM_seq_x1], axis=1)
-
+    # class_pinjie = concatenate([tag_embedding, BiLSTM_seq_x1], axis=1)
+    class_pinjie = Lambda(lambda x: K.concatenate([x[0],x[1]], axis=1))([tag_embedding, BiLSTM_seq_x1])
     class_BiLSTM = Bidirectional(LSTM(200, activation='tanh'), merge_mode='concat')(class_pinjie)
     class_BiLSTM = Dropout(0.5)(class_BiLSTM)
     class_BiLSTM = Dense(100, activation='tanh',name='class_BiLSTM')(class_BiLSTM)
