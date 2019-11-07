43e79aba06d57f0fbd1882f7c5dcd811b175b193
TJUNLP <jiashengbin0501@163.com>
2019-11-07 21:24:24 +0800

    11-7-3

---
+++ TextBlob.py
@@ -0,0 +1,18 @@
+from textblob import TextBlob
+from textblob import Word
+from textblob.wordnet import VERB, NOUN
+
+
+
+
+
+
+word = Word("mouth")
+print(word.synsets)
+define = word.definitions
+print(define)
+
+wiki = TextBlob("Python is a high-level, general-purpose programming language.")
+print(wiki.tags)
+print(wiki.noun_phrases)
+
