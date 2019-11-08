4d8498854c6799cbcd6a77028d74dd93368b0764
TJUNLP <jiashengbin0501@163.com>
2019-11-08 14:36:11 +0800

    11-8-7

--- TrainModel_Siamese_SentPair_triloss_H_635.py
+++ TrainModel_Siamese_SentPair_triloss_H_635.py
@@ -15,6 +15,7 @@ import ProcessData_Siamese_SentPair
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 from NNstruc.NN_Siamese_SentTriplet import Model_BiLSTM_SentPair_tripletloss_Hloss_05_at01_allexp_2m
+from NNstruc.NN_Siamese_SentTriplet import Model_BiLSTM_SentPair_tripletloss_01_
 
 import keras
 
@@ -456,6 +457,20 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                                                   batch_size=batch_size,
                                                   margin=margin, at_margin=at_margin)
 
+    if modelname is 'Model_BiLSTM_SentPair_tripletloss_01_':
+        margin = 0.1
+        at_margin = 0.05
+        nn_model = Model_BiLSTM_SentPair_tripletloss_01_(wordvocabsize=wordvocabsize,
+                                                  posivocabsize=posivocabsize,
+                                                  charvocabsize=charvocabsize,
+                                                    tagvocabsize=tagvocabsize,
+                                                  word_W=word_W, posi_W=posi_W, char_W=char_W, tag_W=tag_W,
+                                                  input_sent_lenth=input_sent_lenth,
+                                                  input_maxword_length=max_c,
+                                                  w2v_k=w2v_k, posi2v_k=posi2v_k, c2v_k=c2v_k, tag2v_k=tag2v_k,
+                                                  batch_size=batch_size,
+                                                  margin=margin, at_margin=at_margin)
+
 
     return nn_model
 
@@ -504,6 +519,7 @@ if __name__ == "__main__":
     modelname = 'Model_BiLSTM_SentPair_tripletloss_Hloss_08_at01_allexp_2m'
     modelname = 'Model_BiLSTM_SentPair_tripletloss_Hloss_01_at03_allexp_2m'
     modelname = 'Model_BiLSTM_SentPair_tripletloss_Hloss_01_at005_allexp_2m'
+    modelname = 'Model_BiLSTM_SentPair_tripletloss_01_'
 
     print(modelname)
 
