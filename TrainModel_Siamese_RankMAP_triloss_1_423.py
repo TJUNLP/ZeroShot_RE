64ceb6ff14a3dbd29ff1b28a62fb6d383495e11a
TJUNLP <jiashengbin0501@163.com>
2019-11-16 22:35:03 +0800

    11-16-4

--- TrainModel_Siamese_RankMAP_triloss_1_423.py
+++ TrainModel_Siamese_RankMAP_triloss_1_423.py
@@ -15,6 +15,7 @@ import ProcessData_Siamese_SentPair
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 from NNstruc.NN_Siamese import Model_ONBiLSTM_RankMAP_three_fourloss_1
+from NNstruc.NN_Siamese import Model_ONBiLSTM_RankMAP_two_fourloss_1
 
 import keras
 
@@ -270,6 +271,20 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                                                   w2v_k=w2v_k, posi2v_k=posi2v_k, c2v_k=c2v_k, tag2v_k=tag2v_k,
                                                   batch_size=batch_size, margin=margin, at_margin=at_margin)
 
+    if modelname is 'Model_ONBiLSTM_RankMAP_two_fourloss_01_423':
+        margin = 0.1
+        at_margin = 0.1
+
+        nn_model = Model_ONBiLSTM_RankMAP_two_fourloss_1(wordvocabsize=wordvocabsize,
+                                                  posivocabsize=posivocabsize,
+                                                  charvocabsize=charvocabsize,
+                                                    tagvocabsize=tagvocabsize,
+                                                  word_W=word_W, posi_W=posi_W, char_W=char_W, tag_W=tag_W,
+                                                  input_sent_lenth=input_sent_lenth,
+                                                  input_maxword_length=max_c,
+                                                  w2v_k=w2v_k, posi2v_k=posi2v_k, c2v_k=c2v_k, tag2v_k=tag2v_k,
+                                                  batch_size=batch_size, margin=margin, at_margin=at_margin)
+
 
     return nn_model
 
@@ -311,6 +326,7 @@ if __name__ == "__main__":
     maxlen = 100
 
     modelname = 'Model_ONBiLSTM_RankMAP_three_fourloss_01_423'
+    modelname = 'Model_ONBiLSTM_RankMAP_two_fourloss_01_423'
 
     print(modelname)
 
