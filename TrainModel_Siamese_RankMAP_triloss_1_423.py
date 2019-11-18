8210f077fff5c63c04097cffefeddf428e6acccf
TJUNLP <jiashengbin0501@163.com>
2019-11-18 11:28:34 +0800

    11-18-2

--- TrainModel_Siamese_RankMAP_triloss_1_423.py
+++ TrainModel_Siamese_RankMAP_triloss_1_423.py
@@ -375,7 +375,7 @@ if __name__ == "__main__":
 
 
 
-    for inum in range(0, 3):
+    for inum in range(3, 6):
 
         tagDict_train, tagDict_dev, tagDict_test, \
         word_vob, word_id2word, word_W, w2v_k, \
