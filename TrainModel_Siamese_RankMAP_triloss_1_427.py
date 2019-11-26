2cda991f451a6707278a32e72c6b5002d86cfe74
TJUNLP <jiashengbin0501@163.com>
2019-11-26 13:20:22 +0800

    11-26-6

--- TrainModel_Siamese_RankMAP_triloss_1_427.py
+++ TrainModel_Siamese_RankMAP_triloss_1_427.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 config = tf.ConfigProto(allow_soft_placement=True)
 #最多占gpu资源的70%
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
 #开始不会给tensorflow全部gpu资源 而是按需增加
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
