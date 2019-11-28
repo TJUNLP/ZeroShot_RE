58ce68af0a5c804b6c5a688a257386f805116deb
TJUNLP <jiashengbin0501@163.com>
2019-11-28 19:40:49 +0800

    11-28-1

--- NNstruc/NN_Siamese.py
+++ NNstruc/NN_Siamese.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-from keras.layers.core import Dropout,RepeatVector, Reshape
+from keras.layers.core import Dropout, RepeatVector, Reshape, Activation
 from keras.layers.merge import concatenate, add, subtract, average, maximum
 from keras.layers import TimeDistributed, Input, Bidirectional, Dense, Embedding, LSTM, Conv1D, GlobalMaxPooling1D
 from keras.models import Model
@@ -15,6 +15,7 @@ from keras.engine import Layer
 from tensorflow.python.framework import ops
 import tensorflow as tf
 import flipGradientTF
+
 
 def Model_ONBiLSTM_directMAP_tripletloss_Hloss_05_at01_allexp_2m(wordvocabsize, posivocabsize, charvocabsize, tagvocabsize,
                      word_W, posi_W, char_W, tag_W,
@@ -600,6 +601,121 @@ def Model_ONBiLSTM_RankMAP_three_triloss_1(wordvocabsize, posivocabsize, charvoc
     # at_cos = Dot(axes=-1, normalize=True, name='at_cos')([tag_embedding_p, tag_embedding_n])
     anchor_cos = Dot(axes=-1, normalize=True, name='anchor_cos')([BiLSTM_x1, tag_embedding_a])
     anchor_wrong_cos = Dot(axes=-1, normalize=True, name='anchor_wrong_cos')([BiLSTM_x1, tag_embedding_n0])
+
+    # margin = 1.
+    # margin = 0.5
+    # margin = 0.1
+    # at_margin = 0.1
+    gamma = 2
+
+    loss = Lambda(lambda x: K.relu(margin1 + x[0] - x[1]) +
+                            K.relu(margin2 + x[2] - x[3]) +
+                            K.relu(margin3 + x[1] - x[3]))\
+        ([wrong_cos, right_cos, anchor_wrong_cos, anchor_cos])
+    mymodel = Model([word_input_sent_x1, input_e1_posi_x1, input_e2_posi_x1, char_input_sent_x1,
+                     input_tag_p, input_tag_n, input_tag_a, input_tag_n0], loss)
+
+    mymodel.compile(loss=lambda y_true,y_pred: y_pred, optimizer=optimizers.Adam(lr=0.001))
+
+    return mymodel
+
+
+def Model_ONBiLSTM_Atten_RankMAP_three_triloss_1(wordvocabsize, posivocabsize, charvocabsize, tagvocabsize,
+                     word_W, posi_W, char_W, tag_W,
+                     input_sent_lenth, input_maxword_length,
+                     w2v_k, posi2v_k, c2v_k, tag2v_k,
+                    batch_size=32, margin1=0.1, margin2=0.1, margin3=0.1):
+
+    word_input_sent_x1 = Input(shape=(input_sent_lenth,), dtype='int32')
+
+    word_embedding_sent_layer = Embedding(input_dim=wordvocabsize + 1,
+                                    output_dim=w2v_k,
+                                    input_length=input_sent_lenth,
+                                    mask_zero=True,
+                                    trainable=True,
+                                    weights=[word_W])
+    word_embedding_sent_x1 = word_embedding_sent_layer(word_input_sent_x1)
+    word_embedding_sent_x1 = Dropout(0.25)(word_embedding_sent_x1)
+
+
+    char_input_sent_x1 = Input(shape=(input_sent_lenth, input_maxword_length,), dtype='int32')
+
+    char_embedding_sent_layer = TimeDistributed(Embedding(input_dim=charvocabsize,
+                               output_dim=c2v_k,
+                               batch_input_shape=(batch_size, input_sent_lenth, input_maxword_length),
+                               mask_zero=False,
+                               trainable=True,
+                               weights=[char_W]))
+
+    char_embedding_sent_x1 = char_embedding_sent_layer(char_input_sent_x1)
+
+
+    char_cnn_sent_layer = TimeDistributed(Conv1D(50, 3, activation='relu', padding='valid'))
+
+    char_embedding_sent_x1 = char_cnn_sent_layer(char_embedding_sent_x1)
+    char_embedding_sent_x1 = TimeDistributed(GlobalMaxPooling1D())(char_embedding_sent_x1)
+    char_embedding_sent_x1 = Dropout(0.25)(char_embedding_sent_x1)
+
+    input_e1_posi_x1 = Input(shape=(input_sent_lenth,), dtype='int32')
+
+    input_e2_posi_x1 = Input(shape=(input_sent_lenth,), dtype='int32')
+
+    embedding_posi_layer = Embedding(input_dim=posivocabsize,
+                                    output_dim=posi2v_k,
+                                    input_length=input_sent_lenth,
+                                    mask_zero=False,
+                                    trainable=False,
+                                    weights=[posi_W])
+
+    embedding_e1_posi_x1 = embedding_posi_layer(input_e1_posi_x1)
+    embedding_e2_posi_x1 = embedding_posi_layer(input_e2_posi_x1)
+
+
+    # BiLSTM_layer = Bidirectional(LSTM(100, activation='tanh'), merge_mode='ave')
+    ONBiLSTM_layer = Bidirectional(ONLSTM(100, chunk_size=5, recurrent_dropconnect=0.2, return_sequences=True), merge_mode='concat')
+
+
+
+    embedding_x1 = concatenate([word_embedding_sent_x1, char_embedding_sent_x1,
+                                embedding_e1_posi_x1, embedding_e2_posi_x1], axis=-1)
+    ONBiLSTM_x1 = ONBiLSTM_layer(embedding_x1)
+    ONBiLSTM_x1 = Dropout(0.25)(ONBiLSTM_x1)
+
+    attention = Dense(1, activation='tanh')(ONBiLSTM_x1)
+    attention = Activation('softmax')(attention)
+    Attention = Lambda(lambda x: x[0] * x[1])([attention, ONBiLSTM_x1])
+
+    ONBiLSTM_x2 = Bidirectional(ONLSTM(100, chunk_size=5, recurrent_dropconnect=0.2), merge_mode='ave')(Attention)
+    ONBiLSTM_x2 = Dropout(0.25)(ONBiLSTM_x2)
+
+    input_tag_p = Input(shape=(1,), dtype='int32')
+    input_tag_n = Input(shape=(1,), dtype='int32')
+    input_tag_a = Input(shape=(1,), dtype='int32')
+    input_tag_n0 = Input(shape=(1,), dtype='int32')
+
+    tag_embedding_layer = Embedding(input_dim=tagvocabsize,
+                                    output_dim=tag2v_k,
+                                    input_length=1,
+                                    mask_zero=False,
+                                    trainable=False,
+                                    weights=[tag_W])
+
+    tag_embedding_p = tag_embedding_layer(input_tag_p)
+    tag_embedding_p = Flatten()(tag_embedding_p)
+    tag_embedding_n = tag_embedding_layer(input_tag_n)
+    tag_embedding_n = Flatten()(tag_embedding_n)
+    tag_embedding_a = tag_embedding_layer(input_tag_a)
+    tag_embedding_a = Flatten()(tag_embedding_a)
+    tag_embedding_n0 = tag_embedding_layer(input_tag_n0)
+    tag_embedding_n0 = Flatten()(tag_embedding_n0)
+
+    # distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([BiLSTM_x1, mlp_x2_2])
+    # cos_distance = dot([BiLSTM_x1, BiLSTM_x2], axes=-1, normalize=True)
+    right_cos = Dot(axes=-1, normalize=True, name='right_cos')([ONBiLSTM_x2, tag_embedding_p])
+    wrong_cos = Dot(axes=-1, normalize=True, name='wrong_cos')([ONBiLSTM_x2, tag_embedding_n])
+    # at_cos = Dot(axes=-1, normalize=True, name='at_cos')([tag_embedding_p, tag_embedding_n])
+    anchor_cos = Dot(axes=-1, normalize=True, name='anchor_cos')([ONBiLSTM_x2, tag_embedding_a])
+    anchor_wrong_cos = Dot(axes=-1, normalize=True, name='anchor_wrong_cos')([ONBiLSTM_x2, tag_embedding_n0])
 
     # margin = 1.
     # margin = 0.5
--- TrainModel_Siamese_RankMAP_triloss_1_426.py
+++ TrainModel_Siamese_RankMAP_triloss_1_426.py
@@ -15,7 +15,7 @@ import ProcessData_Siamese_SentPair
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 from NNstruc.NN_Siamese import Model_ONBiLSTM_RankMAP_three_triloss_1
-
+from NNstruc.NN_Siamese import Model_ONBiLSTM_Atten_RankMAP_three_triloss_1
 import keras
 
 
@@ -273,13 +273,31 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                                                   batch_size=batch_size,
                                                   margin1=margin1, margin2=margin2, margin3=margin3)
 
-    if modelname is 'Model_ONBiLSTM_RankMAP_three_triloss_00801015_426':
+    if modelname is 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426':
         margin1 = 0.08
         margin2 = 0.1
-        margin3 = 0.15
+        margin3 = 0.1
 
 
         nn_model = Model_ONBiLSTM_RankMAP_three_triloss_1(wordvocabsize=wordvocabsize,
+                                                  posivocabsize=posivocabsize,
+                                                  charvocabsize=charvocabsize,
+                                                    tagvocabsize=tagvocabsize,
+                                                  word_W=word_W, posi_W=posi_W, char_W=char_W, tag_W=tag_W,
+                                                  input_sent_lenth=input_sent_lenth,
+                                                  input_maxword_length=max_c,
+                                                  w2v_k=w2v_k, posi2v_k=posi2v_k, c2v_k=c2v_k, tag2v_k=tag2v_k,
+                                                  batch_size=batch_size,
+                                                  margin1=margin1, margin2=margin2, margin3=margin3)
+
+
+    if modelname is 'Model_ONBiLSTM_Atten_RankMAP_three_triloss_0080101_426':
+        margin1 = 0.08
+        margin2 = 0.1
+        margin3 = 0.1
+
+
+        nn_model = Model_ONBiLSTM_Atten_RankMAP_three_triloss_1(wordvocabsize=wordvocabsize,
                                                   posivocabsize=posivocabsize,
                                                   charvocabsize=charvocabsize,
                                                     tagvocabsize=tagvocabsize,
@@ -335,6 +353,9 @@ if __name__ == "__main__":
     modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_020201_426'
     modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_010301_426'
     modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_00801015_426'
+    modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426'
+
+    modelname = 'Model_ONBiLSTM_Atten_RankMAP_three_triloss_0080101_426'
 
     print(modelname)
 
