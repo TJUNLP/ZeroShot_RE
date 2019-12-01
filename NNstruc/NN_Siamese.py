660552a80939206694a8ac0fe8770793e5eaab48
TJUNLP <jiashengbin0501@163.com>
2019-12-01 20:27:33 +0800

    12-1-4

--- NNstruc/NN_Siamese.py
+++ NNstruc/NN_Siamese.py
@@ -714,9 +714,9 @@ def Model_ONBiLSTM_RankMAP_three_triloss_1_ed(wordvocabsize, posivocabsize, char
     # at_margin = 0.1
     gamma = 2
 
-    loss = Lambda(lambda x: K.relu(margin1 + x[0] - x[1]) +
-                            K.relu(margin2 + x[2] - x[3]) +
-                            K.relu(margin3 + x[1] - x[3]))\
+    loss = Lambda(lambda x: K.relu(margin1 + x[1] - x[0]) +
+                            K.relu(margin2 + x[3] - x[2]) +
+                            K.relu(margin3 + x[3] - x[1]))\
         ([wrong_cos, right_cos, anchor_wrong_cos, anchor_cos])
     mymodel = Model([word_input_sent_x1, input_e1_posi_x1, input_e2_posi_x1, char_input_sent_x1,
                      input_tag_p, input_tag_n, input_tag_a, input_tag_n0], loss)
