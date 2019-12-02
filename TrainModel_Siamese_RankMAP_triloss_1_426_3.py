012168fd604a8dac8cf26a709e40e1cb62c0e2f8
TJUNLP <jiashengbin0501@163.com>
2019-12-02 14:53:29 +0800

    12-2-1

--- TrainModel_Siamese_RankMAP_triloss_1_426_3.py
+++ TrainModel_Siamese_RankMAP_triloss_1_426_3.py
@@ -244,7 +244,7 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                      batch_size=32):
     nn_model = None
 
-    if modelname is 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_p43':
+    if modelname is 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_p65':
         margin1 = 0.08
         margin2 = 0.1
         margin3 = 0.1
@@ -298,10 +298,11 @@ if __name__ == "__main__":
 
     maxlen = 100
 
-    percent_of_trainset = 43
+    percent_of_trainset = 65
 
     modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_p22'
     modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_p43'
+    modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_p65'
 
 
     print(modelname)
