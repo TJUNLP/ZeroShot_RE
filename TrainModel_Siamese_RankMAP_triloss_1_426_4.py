6a3b956cafa8f9878e78ef5c5e38ef7075f6b310
TJUNLP <jiashengbin0501@163.com>
2019-12-11 16:14:13 +0800

    12-11-9

--- TrainModel_Siamese_RankMAP_triloss_1_426_4.py
+++ TrainModel_Siamese_RankMAP_triloss_1_426_4.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
     maxlen = 100
 
     # 100 place of death  106 father 98 programming language 103 publication date 109 chairperson
-    tag4test = 100
+    tag4test = 109
     percent_of_trainset = 43
 
     modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id100'
