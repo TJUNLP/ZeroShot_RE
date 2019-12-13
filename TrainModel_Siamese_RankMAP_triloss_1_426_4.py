7c7f6b4dc94a1769fdab537e9513c58bc66789a4
TJUNLP <jiashengbin0501@163.com>
2019-12-13 10:31:29 +0800

    12-13-3

--- TrainModel_Siamese_RankMAP_triloss_1_426_4.py
+++ TrainModel_Siamese_RankMAP_triloss_1_426_4.py
@@ -393,6 +393,21 @@ def SelectModel(modelname, wordvocabsize, tagvocabsize, posivocabsize,charvocabs
                                                   batch_size=batch_size,
                                                   margin1=margin1, margin2=margin2, margin3=margin3)
 
+    if modelname is 'Model_ONBiLSTM_directMAP_tripletloss_01_1':
+        margin = 0.1
+        nn_model = Model_ONBiLSTM_directMAP_tripletloss_1(wordvocabsize=wordvocabsize,
+                                                                                    posivocabsize=posivocabsize,
+                                                                                    charvocabsize=charvocabsize,
+                                                                                    tagvocabsize=tagvocabsize,
+                                                                                    word_W=word_W, posi_W=posi_W,
+                                                                                    char_W=char_W, tag_W=tag_W,
+                                                                                    input_sent_lenth=input_sent_lenth,
+                                                                                    input_maxword_length=max_c,
+                                                                                    w2v_k=w2v_k, posi2v_k=posi2v_k,
+                                                                                    c2v_k=c2v_k, tag2v_k=tag2v_k,
+                                                                                    batch_size=batch_size, margin=margin)
+
+
     return nn_model
 
 
@@ -496,7 +511,7 @@ if __name__ == "__main__":
 
     # modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id100_ascend_20'
     # modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id106_ascend_20'
-    modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id109_ascend_20'
+    # modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id109_ascend_20'
     ascend = True
     # modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id100_descend_20'
     ascend = False
@@ -504,6 +519,7 @@ if __name__ == "__main__":
     # modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426_id106_random_20'
 
     # modelname = 'Model_ONBiLSTM_RankMAP_three_triloss_0080101_426'
+    modelname = 'Model_ONBiLSTM_directMAP_tripletloss_01_1'
 
     print(modelname)
 
